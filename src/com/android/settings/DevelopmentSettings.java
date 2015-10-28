9a4becc5a7b516674e35214616f803f4764b565f
George G <kreach3r@users.noreply.github.com>
2015-10-28 20:56:41 +0200

    Development: fix the switch state
    When we changed the default animation values, we forgot this. Having "mHaveDebugSettings" set to true didn't allow the switchbar to work (because it detected changed dev settings).

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -1713,7 +1713,7 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
     private void updateAnimationScaleValue(int which, AnimationScalePreference pref) {
         try {
             float scale = mWindowManager.getAnimationScale(which);
-            if (scale != 1) {
+            if (scale != 0.5) {
                 mHaveDebugSettings = true;
             }
             pref.setScale(scale);
